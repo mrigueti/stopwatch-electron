cffb1cd056c1c425c35269fae05a545bbb23d6a0
Maykel <maykelrigueti93@gmail.com>
2025-07-01 14:39:40 -0300

    app done

---
+++ src/main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    resizable: false,
+  });
+
+  mainWindow.loadFile(path.join(__dirname, "index.html"));
+  mainWindow.setMenu(null);
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
